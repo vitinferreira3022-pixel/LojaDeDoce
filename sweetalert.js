5695508270a0c04932a90325ce1b92d1219e7b36
VICTOR HUGO DE BARROS ALMEIDA FERREIRA <victor.ferreira75@senaisp.edu.br>
2026-03-23 15:30:42 -0300

    Melhoria

--- sweetalert.js
+++ sweetalert.js
@@ -4,33 +4,7 @@ $(document).ready(function () {
         text: "You clicked the button!",
         icon: "success"
     });
-
-    $("#btninfo").click(function () {
-
-        $("#Titulo_danger").hide();
-        $("#Titulo_info").show();
-
-        swal.fire({
-            title: "Botao info clicado",
-            text: "Titulo danger foi escondido",
-            icon: "success"
-        });
-
-
-    });
-    $("#btndanger").click(function () {
-
-        $("#Titulo_info").hide();
-        $("#Titulo_danger").show();
-
-        swal.fire({
-            title: "Botao danger clicado",
-            text: "Titulo info foi escondido",
-            icon: "success"
-        });
-
-
-    });
+    
     $("#chocolates").click(function () {
 
         $("#bala").hide();
